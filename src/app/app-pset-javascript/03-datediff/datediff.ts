7c6a5ed0724d222b6bc6c8a763d3d5481d74737a
Ghiath Alshammaa <ghiathalshammaa@MacBook-Pro-4.local>
2018-03-22 11:41:23 +0100

    finished pset 3

--- src/app/app-pset-javascript/03-datediff/datediff.ts
+++ src/app/app-pset-javascript/03-datediff/datediff.ts
@@ -1,3 +1,27 @@
 export function datediff(first, second): number {
-  return -1;
+
+  var hours = first.getHours() - second.getHours();
+  var dayCount = first.getDate() - second.getDate();
+  
+  if (hours == 0)
+  {
+      if (dayCount == 0 )
+      {
+          if (first.getFullYear() > second.getFullYear() )
+          {
+              dayCount = dayCount + ((first.getFullYear() - second.getFullYear()) * 365);
+              
+          }
+          else
+          {
+              dayCount = (dayCount + ((second.getFullYear() - first.getFullYear()) * 365)) * 1;
+          }
+      }
+  }
+  else
+  {
+      dayCount = dayCount * -1;
+  }
+
+  return dayCount;
 }
