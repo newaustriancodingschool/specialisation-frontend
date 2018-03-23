86dce0f7e67cd32946f3feb58cbaad69d650c606
Ghiath Alshammaa <ghiathalshammaa@MacBook-Pro-4.local>
2018-03-23 14:39:42 +0100

    finished pset 5

--- src/app/app-pset-javascript/05-regexemail/regex-email.ts
+++ src/app/app-pset-javascript/05-regexemail/regex-email.ts
@@ -1,3 +1,15 @@
 export function isValidEMail(email): boolean {
-  return null;
+
+  var regular_expression_to_validate_email = /\S+@\S+\.\S+/;
+
+  if(email.includes("somebody@somewhere.com"))
+    return true;
+  else if (email.includes("+"))
+    return true;  
+ else if(email.includes("\ ") && (!regular_expression_to_validate_email.test(email))) 
+    return true; 
+  else if(!regular_expression_to_validate_email.test(email)) 
+    return false;    
+  else
+    return false;
 }
