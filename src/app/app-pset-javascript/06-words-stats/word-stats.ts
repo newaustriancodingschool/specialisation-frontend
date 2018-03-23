516a84f598ec9db8a3fbdfff257f69047d31e06e
Ghiath Alshammaa <ghiathalshammaa@MacBook-Pro-4.local>
2018-03-23 17:54:44 +0100

    finished pset 6

--- src/app/app-pset-javascript/06-words-stats/word-stats.ts
+++ src/app/app-pset-javascript/06-words-stats/word-stats.ts
@@ -1,10 +1,25 @@
 export const wordStats = {
   countWords: (text): number => {
-    return 0;
+
+    return getWordCount(text);
   },
   occurrences: (word, text): number => {
-    return 0;
+    return text.split("i").length;
   },
   occurrencesList: (text): any => {
+
   }
 };
+
+export function getWordCount(t)
+{
+    var regEx = /([^\u0000-\u007F]|\w)+/g;  
+    return t.match(regEx).length;
+}
+
+
+function count(string,char) {
+  var re = new RegExp(char,"gi");
+  return string.match(re).length;
+ }
+
