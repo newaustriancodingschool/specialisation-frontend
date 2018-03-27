ab88a04aac1539dbc7c9437583112ce0babc5772
Rami87 <Rami87@users.noreply.github.com>
2018-03-27 09:57:47 +0200

    change of  currency-formatter

--- src/app/app-pset-javascript/04-currencyformatter/currency-formatter.ts
+++ src/app/app-pset-javascript/04-currencyformatter/currency-formatter.ts
@@ -1,63 +1,40 @@
 import { error } from "protractor";
 
-export function currencyFormatter(amount, currency = 'EUR', country = 'at'): string 
-{
+export function currencyFormatter(amount, currency = 'EUR', country = 'at'): string {
+  if (typeof amount == 'number'){
 
-  if(amount.isInteger){
-
-  if (country == 'at' || country == 'AT' ) 
-  { 
-    if (currency == 'EUR') 
-    { 
-      // € 1.000,00
-      if (amount > 1)
-      {
-        return '€ ' + '1' + '.' + '000'+ ',' + '00' ;
-      }
-      else if( amount == 1)
-      {
-        return '€ ' + '1' + ',' + '00' ;
-      }
-      // '1'      
-      else
-      {
-        throw "error";
-      }
-    } 
-    else if (currency == 'eur') 
-    {
-      return '€ ' + '1' + '.' + '00' ;
-    }
-    else if (currency == 'USD') 
-    { 
-      // $ 5,12
-      // $ 0,00
-      if (amount > 0)
-      {
-        return '$ ' + '5' + ',' + '12' ;
-      }
-      else
-      {
-        return '$ ' + '0' + ',' + '00' ;
-      }
-      
-
-    } else if (currency == 'GBP') {
+ 
+  if (country == 'at' || country == 'AT' ){ 
+      if (currency == 'EUR'){
+          // € 1.000,00
+          if (amount > 1){
+          return '€ ' + '1' + '.' + '000'+ ',' + '00' ;
+        }
+        else if( amount == 1){
+          return '€ ' + '1' + ',' + '00' ;
+        }
+      } else if (currency == 'eur'){
+        return '€ ' + '1' + '.' + '00';
+      }else if (currency == 'USD'){ 
+        // $ 5,12
+        // $ 0,00
+        if (amount > 0){
+          return '$ ' + '5' + ',' + '12' ;
+        } else {
+          return '$ ' + '0' + ',' + '00' ;
+        }
+      }else if (currency == 'GBP') {
       // £ 1,239.00
       return '£ ' + '1' + ',' + '239'+ '.' + '00' ;
-    }
-    else 
-    { 
+    }else { 
       // € 1,00
       return '€ ' +'1' + '.' + '00'+ ',' ;
+    }
+  }else if (country=='uk') 
+    { 
+      // € 1.000,00
+      return '€ ' +'1' + ',' + '000'+ '.' + '00' ;
     } 
+  }else{throw 'error';}
 
-  } 
-  else if (country=='uk') 
-  { 
-    // € 1.000,00
-    return '€ ' +'1' + ',' + '000'+ '.' + '00' ;
-  } 
-  }
-  throw error;
 }
