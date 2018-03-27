b7a8a6a66477e141d1ebe2da83287696cca70d0d
Alex Hartveld <alex@MacBook-RC-5.local>
2018-03-27 13:25:52 +0200

    small change to datediff

--- src/app/app-pset-javascript/03-datediff/datediff.ts
+++ src/app/app-pset-javascript/03-datediff/datediff.ts
@@ -1,7 +1,5 @@
 export function datediff(first, second): number {
   // get the UTC format. 
   let milliDiff = first.valueOf() - second.valueOf();
-  return Math.trunc(Math.abs(milliDiff / (1000*60*60*24)));
-  // then start subtracting hours in a loop
-  // then return the number of days
+  return Math.ceil(Math.abs(milliDiff / (1000*60*60*24)));
 }
