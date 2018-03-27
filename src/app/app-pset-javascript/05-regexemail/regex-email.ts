24fa593f0606e97fd8dd2a8bd27916bfb0c1be92
farah7tawfeek <farahalrubaye7@gmail.com>
2018-03-27 13:32:47 +0200

    last update

--- src/app/app-pset-javascript/05-regexemail/regex-email.ts
+++ src/app/app-pset-javascript/05-regexemail/regex-email.ts
@@ -1,3 +1,11 @@
 export function isValidEMail(email): boolean {
-  return null;
+ 
+var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//if (email.includes("\ "))
+  //  return true;  
+return regex.test(String(email).toLowerCase());
+
+
 }
--- src/app/app-pset-javascript/06-words-stats/word-stats.ts
+++ src/app/app-pset-javascript/06-words-stats/word-stats.ts
@@ -1,10 +1,22 @@
+import { text } from "@angular/core";
+
 export const wordStats = {
   countWords: (text): number => {
-    return 0;
+    return text.split(' ').length;
   },
   occurrences: (word, text): number => {
-    return 0;
+  var sentence = "I write this text, where I don\'t use lower cases for I";
+  var count = (sentence.match(/I/g) || []).length;
+  return count;
   },
-  occurrencesList: (text): any => {
-  }
+  occurrencesList: (text): any => {};
+   /* var arr = [];
+    arr =  text.split(" ");
+    var occurrencesarr = { };
+
+    for (var i = 0, j = arr.length; i < j; i++) {
+      occurrencesarr[arr[i]] = (occurrencesarr[arr[i]] || 0) + 1;
+    }
+    return occurrencesarr;
+  }   
 };
