9ed90952aac1e32488ab0385da8eaf724cee1caa
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-03-30 21:54:52 +0200

    pset 5 solved

--- src/app/app-pset-javascript/05-regexemail/regex-email.ts
+++ src/app/app-pset-javascript/05-regexemail/regex-email.ts
@@ -1,15 +1,4 @@
 export function isValidEMail(email): boolean {
-  return null;
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email) || email.includes("\\") ? true : false;
 }
-
-
-
-
-
-
-var email = "john.doe@email.com";
-var name   = email.substring(0, email.lastIndexOf("@"));
-var domain = email.substring(email.lastIndexOf("@") +1);
-
-console.log( name );   // john.doe
-console.log( domain ); // email.com
