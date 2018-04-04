5072b7d865cc86029657d1ec41bad799a02dfc2b
farah7tawfeek <farahalrubaye7@gmail.com>
2018-04-04 18:12:02 +0200

    Pset5 almost finished

--- src/app/app-pset-javascript/06-words-stats/word-stats.ts
+++ src/app/app-pset-javascript/06-words-stats/word-stats.ts
@@ -17,20 +17,23 @@ export const wordStats = {
       this.count = count
     };
 
-    //Convert the text to lower 
+    //Convert the text to lower case to calculate all the rose together
     let LowerCasetext = text.toLowerCase();
-    let sentence = [] ;
-    
+
+    let sentence = [];
     sentence = LowerCasetext.split(" ");
     let sortedArr = [],
     counter = 1;
+    //Sort the array 
     sortedArr = sentence.sort();
+    //Two loops for matching and calculate the same word
     for (var i = 0; i < sortedArr.length; i = i + counter) {
       counter = 1; //to reset the counter
       for (var j = i + 1; j < sortedArr.length; j++) {
         if (sortedArr[i] === sortedArr[j])
         counter++;  
       }
+     // I got the right result but I couldn't get the right form 
     var obj = new Object(sortedArr[i], counter);
     arrObj.push(obj);
     }
